08f0e4b2f88ad60385a46df28ec0d54213f2f705
fabiano_hatori <fabiano@joiss.com>
2014-05-30 13:55:33 -0300

    Initial Commit

---
+++ Out of this world/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of this world
+//
+//  Created by JoissDev on 30/05/14.
+//  Copyright (c) 2014 joiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this world/OWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWViewController.h
+//  Out of this world
+//
+//  Created by JoissDev on 30/05/14.
+//  Copyright (c) 2014 joiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWViewController : UIViewController
+
+@end
